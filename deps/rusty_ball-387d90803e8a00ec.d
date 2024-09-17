fabd87befaeba6924a36f9a63665aff1c16bd296
Soma Deme <demesoma@gmail.com>
2024-09-17 22:48:10 +0200

    Now with responsive window resize capabilities

---
+++ deps/rusty_ball-387d90803e8a00ec.d
@@ -0,0 +1,5 @@
+C:\Users\Soma\repos\rusty_ball\target\wasm32-unknown-unknown\release\deps\rusty_ball-387d90803e8a00ec.wasm: src/main.rs
+
+C:\Users\Soma\repos\rusty_ball\target\wasm32-unknown-unknown\release\deps\rusty_ball-387d90803e8a00ec.d: src/main.rs
+
+src/main.rs:
--- deps/rusty_ball-e5eb4368cee1360d.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Soma\repos\rusty_ball\target\wasm32-unknown-unknown\release\deps\rusty_ball-e5eb4368cee1360d.wasm: src/main.rs
-
-C:\Users\Soma\repos\rusty_ball\target\wasm32-unknown-unknown\release\deps\rusty_ball-e5eb4368cee1360d.d: src/main.rs
-
-src/main.rs:
